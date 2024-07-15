7d180412f63b927ca2e64e639ce13d1db086b39b
Mounir <mounir.abdous@nventive.com>
2024-07-15 10:39:15 -0400

    chore: terraform reference naming

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ data "azurerm_resource_group" "rg_env" {
   name = "rg-${var.environment}-${var.project_short_name}"
 }
 
-resource "azurerm_storage_account" "sa_webapp" {
+resource "azurerm_storage_account" "default" {
   name                      = "sa${var.project_short_name}${var.environment}"
   resource_group_name       = data.azurerm_resource_group.rg_env.name
   location                  = data.azurerm_resource_group.rg_env.location
@@ -22,7 +22,7 @@ resource "azurerm_storage_account" "sa_webapp" {
   }
 }
 
-resource "azurerm_cdn_profile" "cdnp" {
+resource "azurerm_cdn_profile" "default" {
   name                = "cdnp-${var.environment}-${var.project_short_name}"
   resource_group_name = data.azurerm_resource_group.rg_env.name
   location            = data.azurerm_resource_group.rg_env.location
@@ -34,18 +34,18 @@ resource "azurerm_cdn_profile" "cdnp" {
   }
 }
 
-resource "azurerm_cdn_endpoint" "cdne" {
+resource "azurerm_cdn_endpoint" "default" {
   name                          = "cdne-${var.environment}-${var.project_short_name}"
-  profile_name                  = azurerm_cdn_profile.cdnp.name
+  profile_name                  = azurerm_cdn_profile.default.name
   location                      = data.azurerm_resource_group.rg_env.location
   resource_group_name           = data.azurerm_resource_group.rg_env.name
   optimization_type             = "GeneralWebDelivery"
   querystring_caching_behaviour = "UseQueryString"
-  origin_host_header            = replace(azurerm_storage_account.sa_webapp.primary_web_host, "https://", "")
+  origin_host_header            = replace(azurerm_storage_account.default.primary_web_host, "https://", "")
 
   origin {
     name       = var.project_short_name
-    host_name  = replace(azurerm_storage_account.sa_webapp.primary_web_host, "https://", "")
+    host_name  = replace(azurerm_storage_account.default.primary_web_host, "https://", "")
     https_port = "443"
   }
 
