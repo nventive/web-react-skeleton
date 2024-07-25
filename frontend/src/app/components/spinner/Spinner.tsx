fc99148e0c8b5f1cc02d4c1b11f9612068b642f8
Charles Héon <charles.heon@nventive.com>
2024-07-25 10:57:53 -0400

    fix: switch back to pure css loader

--- frontend/src/app/components/spinner/Spinner.tsx
+++ frontend/src/app/components/spinner/Spinner.tsx
@@ -1,10 +1,5 @@
-import CircleNotch from "@icons/CircleNotch";
 import "./spinner.scss";
 
 export default function Spinner() {
-  return (
-    <div className="spinner">
-      <CircleNotch />
-    </div>
-  );
+  return <div className="spinner"></div>;
 }
