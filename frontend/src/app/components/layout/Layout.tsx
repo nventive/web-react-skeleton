c889c53d96026693a8b53eb3a02e852a877a85c9
Charles Héon <charles.heon@nventive.com>
2024-07-29 09:01:17 -0400

    fix: changes debugbanner place

--- frontend/src/app/components/layout/Layout.tsx
+++ frontend/src/app/components/layout/Layout.tsx
@@ -1,7 +1,6 @@
 import classnames from "classnames";
 import { ReactNode } from "react";
 import "./layout.scss";
-import DebugBanner from "@containers/debugBanner/DebugBanner";
 
 interface ILayout {
   children: ReactNode;
@@ -10,22 +9,17 @@ interface ILayout {
 
 function Container({ children, className }: ILayout) {
   return (
-    <>
-      <DebugBanner />
-      <main className={classnames("layout__container", className)}>
-        <div className="layout__container-content">{children}</div>
-      </main>
-    </>
+    <main className={classnames("layout__container", className)}>
+      <div className="layout__container-content">{children}</div>
+    </main>
   );
 }
 
 function Auth({ children, className }: ILayout) {
   return (
-    <>
-      <main className={classnames("layout__auth", className)}>
-        <div className="layout__auth-content">{children}</div>
-      </main>
-    </>
+    <main className={classnames("layout__auth", className)}>
+      <div className="layout__auth-content">{children}</div>
+    </main>
   );
 }
 
