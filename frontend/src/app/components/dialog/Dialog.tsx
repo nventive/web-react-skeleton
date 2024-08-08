b89cc97117eeeb2d74b657f80df969eb71c15d07
Mounir <mounir.abdous@nventive.com>
2024-08-08 10:18:38 -0400

    fix: slide

--- frontend/src/app/components/dialog/Dialog.tsx
+++ frontend/src/app/components/dialog/Dialog.tsx
@@ -1,8 +1,6 @@
 import Slide from "@components/slide/Slide";
 import { DialogProps, Dialog as MuiDialog, styled } from "@mui/material";
-import { TransitionProps } from "@mui/material/transitions";
 import style from "@styles/style.module.scss";
-import { forwardRef, JSXElementConstructor, ReactElement, Ref } from "react";
 
 const StyledMuiDialog = styled(MuiDialog)({
   "& .MuiDialog-paper": {
@@ -10,20 +8,14 @@ const StyledMuiDialog = styled(MuiDialog)({
   },
 });
 
-const Transition = forwardRef(function Transition(
-  props: TransitionProps & {
-    children: ReactElement<unknown, JSXElementConstructor<unknown>>;
-  },
-  ref: Ref<unknown>,
-) {
-  return <Slide direction="up" ref={ref} {...props} timeout={500} />;
-});
-
 export default function Dialog({ ...props }: DialogProps) {
   return (
     <StyledMuiDialog
       disableScrollLock
-      TransitionComponent={Transition}
+      TransitionComponent={Slide}
+      TransitionProps={{
+        timeout: 500,
+      }}
       {...props}
     >
       {props.children}
--- frontend/src/app/components/slide/Slide.tsx
+++ frontend/src/app/components/slide/Slide.tsx
@@ -1,9 +1,13 @@
 import { Slide as MuiSlide, SlideProps } from "@mui/material";
+import { forwardRef, Ref } from "react";
 
-export default function Slide({
-  direction = "up",
-  timeout = 500,
-  ...props
-}: SlideProps) {
-  return <MuiSlide direction={direction} timeout={timeout} {...props} />;
-}
+const Slide = forwardRef(function Slide(
+  { direction = "up", timeout = 5000, ...props }: SlideProps,
+  ref: Ref<unknown>,
+) {
+  return (
+    <MuiSlide ref={ref} direction={direction} timeout={timeout} {...props} />
+  );
+});
+
+export default Slide;
